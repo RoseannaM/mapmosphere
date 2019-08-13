bf79cacb043490e6247f0d39e6b7b78daeb4ddc2
Anonymous <mcfarlrose@gmail.com>
2019-08-13 16:33:36 -0700

    Finally got modal working...Note to self, the constructor runs ONLY ONCE

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ function App() {
   return (
     <div id="main">
     <Navbar/>
-    <MessageForm/>
-
-    <Map/>
+    {/* <Map/> */}
     </div>
     
   );
---
+++ src/Modal.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export default class Modal extends Component {
+  constructor(props) {
+    super(props);
+    this.test = this.props;
+  }
+
+  render() {
+    const showHideClassName = this.props.showMessageForm ? 'modal display-block' : 'modal display-none';  
+    const { handleClose, children} = this.props;
+    return (
+      <div className={showHideClassName}>
+        <section className="modal-main">
+          {children}
+          <button onClick={handleClose}>close</button>
+        </section>
+      </div>
+    );
+  }
+}
